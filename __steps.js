9637eeb31a901bfe71a18ec24ca3defd472bb70b
alamin1246 <hossainalamin1246@gmail.com>
2022-04-21 14:05:01 +0600

    added steps

---
+++ __steps.js
@@ -0,0 +1,27 @@
+/* 
+* ---------------------------------
+* Basic Server Setup
+* ----------------------------------
+* 1. create a folder (manually or mkdir)
+* 2. open command line to that folder
+* 3. yarn init
+* 4. create index.js
+* 5. yarn add express
+* ---------------------------------
+* For Express/ node server
+* ---------------------------------
+* 1.  require express
+* 2. create app variable
+* 3. declare port
+* 4. set app.get('/')
+* 5. listen to port
+* 6. node index.js
+* 7. check your browser for that port
+* -----------------------
+* get
+* -----------------------
+* 1. cors middleware
+*------------------------------------
+* post
+* -----------------------------------
+*/
